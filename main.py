524f3682b61873472542453d7e303e55a6cff27b
Swamyrangareddy Muthumula <127617789+swamyrangareddy@users.noreply.github.com>
2024-09-12 10:45:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,23 +16,24 @@ class MultiApp:
             "function" : function
         })
     def run():
-         # Radio button selection
+        # Radio button selection
         with st.sidebar:
             app = st.radio(
-            label="Select",
-            options=["Folder", "Single"],
+            label="Choose File Selection Method",
+            options=["Provide Folder Path", "Upload File"],
             
         )
 
-       # with st.sidebar:
-       #     app = option_menu(
-       #         menu_title ="Select",
-       #         options =["Folder", "Single"],
-       #         icons=["folder","file-earmark-arrow-down"],
-       #         default_index=0,)
+    # with st.sidebar:
+    #     app = option_menu(
+    #         menu_title ="Select",
+    #         options =["Folder", "Single"],
+    #         icons=["folder","file-earmark-arrow-down"],
+    #         default_index=0,)
         
-        if app == "Single":
-            Single.app()
-        if app=="Folder":
+        if app == "Upload File":
+            with st.spinner("Processing folder..."):
+                Single.app()
+        if app=="Provide Folder Path":
             Folder.app()
     run()
